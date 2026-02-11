c9c68eb9c2e9620e7dce7104ed4b929a110453b0
Samuel-Valentini <samuelvitalia@gmail.com>
2026-02-11 17:34:22 +0100

    search in homepage

--- src/component/NfMain.jsx
+++ src/component/NfMain.jsx
@@ -1,34 +1,50 @@
-import { Component } from "react";
+import { useState } from "react";
 import NfTitleBar from "./NfTitleBar";
 import NfCarouselBar from "./NfCarouselBar";
+import SearchBar from "./SearchBar";
+import { Alert } from "react-bootstrap";
 
-class NfMain extends Component {
-    render() {
-        return (
-            <>
-                {" "}
-                <main>
-                    <section className="container">
-                        <NfTitleBar />
+const NfMain = () => {
+    const [search, setSearch] = useState("");
+    const titleText = `Risultati di ricerca per: ${search}`;
+
+    return (
+        <>
+            {" "}
+            <main>
+                <section className="container">
+                    <NfTitleBar />
+                    <SearchBar search={search} setSearch={setSearch} />
+                    {search === "" ? (
+                        <Alert variant="success" className="w-50 mx-auto mt-5">
+                            Effettua una ricerca, i risultati compariranno in
+                            automatico
+                        </Alert>
+                    ) : (
                         <NfCarouselBar
-                            title="Star Wars"
-                            id="starWars"
-                            search="Star Wars"
+                            title={titleText}
+                            id="search-bar"
+                            search={search}
                         />
-                        <NfCarouselBar
-                            title="Ghost in the Shell"
-                            id="ghostInTheShell"
-                            search="Ghost in the Shell"
-                        />
-                        <NfCarouselBar
-                            title="Star Trek"
-                            id="starTrek"
-                            search="Star Trek"
-                        />
-                    </section>
-                </main>
-            </>
-        );
-    }
-}
+                    )}
+                    <NfCarouselBar
+                        title="Star Wars"
+                        id="starWars"
+                        search="Star Wars"
+                    />
+                    <NfCarouselBar
+                        title="Ghost in the Shell"
+                        id="ghostInTheShell"
+                        search="Ghost in the Shell"
+                    />
+                    <NfCarouselBar
+                        title="Star Trek"
+                        id="starTrek"
+                        search="Star Trek"
+                    />
+                </section>
+            </main>
+        </>
+    );
+};
 export default NfMain;
